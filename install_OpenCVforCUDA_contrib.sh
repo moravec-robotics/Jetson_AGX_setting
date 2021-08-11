ccec0600cf7f9d5f968277eeed4169f7cb0f6ad8
Jeong Seok Hoon <87402762+tonymoravec@users.noreply.github.com>
2021-08-11 18:19:57 +0900

    Update install_OpenCVforCUDA_contrib.sh

--- install_OpenCVforCUDA_contrib.sh
+++ install_OpenCVforCUDA_contrib.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 
-echo "[Install OpenCV 4.5.1 for CUDA, OpenCV_contrib]"
+echo "[**********Install OpenCV 4.5.1 for CUDA, OpenCV_contrib**********]"
 sudo sh -c "echo '/usr/local/cuda/lib64' >> /etc/ld.so.conf.d/nvidia-tegra.conf"
 sudo ldconfig
 
-echo "[Install Dependencies]"
-sudo apt-get install -y \
-build-essential cmake git unzip pkg-config\
-libjpeg-dev libpng-dev libtiff-dev\
-libavcodec-dev libavformat-dev libswscale-dev\
-libgtk2.0-dev libcanberra-gtk*\
-python3-dev python3-numpy python3-pip\
-libxvidcore-dev libx264-dev libgtk-3-dev\
-libtbb2 libtbb-dev libdc1394-22-dev\
-libv4l-dev v4l-utils\
-libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev\
-libavresample-dev libvorbis-dev libxine2-dev\
-libfaac-dev libmp3lame-dev libtheora-dev\
-libopencore-amrnb-dev libopencore-amrwb-dev\
-libopenblas-dev libatlas-base-dev libblas-dev\
-liblapack-dev libeigen3-dev gfortran\
-libhdf5-dev protobuf-compiler\
-libprotobuf-dev libgoogle-glog-dev libgflags-dev
+echo ""
+echo ""
+echo "[**********Install Dependencies**********]"
+sudo apt-get install -y build-essential cmake git 
+sudo apt-get install -y unzip pkg-config 
+sudo apt-get install -y libjpeg-dev libpng-dev libtiff-dev libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev libcanberra-gtk* 
+sudo apt-get install -y python3-dev python3-numpy python3-pip 
+sudo apt-get install -y libxvidcore-dev libx264-dev libgtk-3-dev libtbb2 libtbb-dev libdc1394-22-dev libv4l-dev 
+sudo apt-get install -y v4l-utils libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev 
+sudo apt-get install -y libavresample-dev libvorbis-dev libxine2-dev libfaac-dev libmp3lame-dev 
+sudo apt-get install -y libtheora-dev libopencore-amrnb-dev libopencore-amrwb-dev 
+sudo apt-get install -y libopenblas-dev libatlas-base-dev libblas-dev liblapack-dev libeigen3-dev 
+sudo apt-get install -y gfortran libhdf5-dev protobuf-compiler 
+sudo apt-get install -y libprotobuf-dev libgoogle-glog-dev libgflags-dev
 
-echo "[Download OpenCV]"
+echo ""
+echo ""
+echo "[**********Download OpenCV**********]"
 cd ~
 wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip
 wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
@@ -36,7 +34,9 @@ mv opencv_contrib-4.5.1 opencv_contrib
 rm opencv.zip
 rm opencv_contrib.zip
 
-echo "[Build OpenCV]"
+echo ""
+echo ""
+echo "[**********Build OpenCV**********]"
 cd ~/opencv
 mkdir build
 cd build
@@ -75,7 +75,7 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D OPENCV_GENERATE_PKGCONFIG=ON \
 -D BUILD_EXAMPLES=OFF ..
 
-make -j7
+make -j$(($(nproc)*2-2))
 
 cd ~
 sudo rm -r /usr/include/opencv4/opencv2
@@ -85,4 +85,8 @@ sudo ldconfig
 make clean
 sudo apt-get update
 
-python3 -c "import cv2; print('OpenCV Version : ',cv2.__version__); print('GPU Available : ',cv2.cuda.getCudaEnabledDeviceCount())"
+
+echo ""
+echo ""
+echo "[**********Check Install**********]"
+python3 -c "import cv2; print(cv2.__version__); print(cv2.cuda.getCudaEnabledDeviceCount())"
